bdfe6b5304e73f52c86a01660408251830844cae
Chaahat <arorac2@mymacewan.ca>
2023-05-18 20:03:49 -0600

    updated code

--- vampire.js
+++ vampire.js
@@ -6,39 +6,66 @@ class Vampire {
     this.creator = null;
   }
 
-  /** Simple tree methods **/
-
-  // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
-  // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
-  // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      numberOfVampires++;
+      currentVampire = currentVampire.creator;
+    }
+
+    return numberOfVampires;
   }
 
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
-  /** Stretch **/
-
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return this;
+    }
 
+    const thisAncestors = this.getAncestors();
+    const otherAncestors = vampire.getAncestors();
+
+    if (!thisAncestors || !otherAncestors) {
+      return null;
+    }
+
+    let commonAncestor = null;
+
+    for (let i = 0; i < thisAncestors.length; i++) {
+      if (otherAncestors.includes(thisAncestors[i])) {
+        commonAncestor = thisAncestors[i];
+        break;
+      }
+    }
+
+    return commonAncestor || this; 
+  }
+
+  getAncestors() {
+    const ancestors = [];
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      ancestors.push(currentVampire.creator);
+      currentVampire = currentVampire.creator;
+    }
+
+    return ancestors;
   }
 }
 
 module.exports = Vampire;
-
